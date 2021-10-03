c017807708f4fd5c6a16dd739c00820430950bad
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-10-03 11:27:29 +0530

    Insertion Sort

---
+++ Algorithms/InsertionSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+   public class InsertionSort
+    {
+        public void Insert()
+        {
+            Console.WriteLine("Insert the Sentence You Want To Sort");
+            string input = Console.ReadLine();
+
+            string[] array = input.Split(" ");
+            int n = array.Length;
+
+            for(int i=1; i < n; ++i)
+            {
+                string key = array[i];
+                int  j = i - 1;
+
+                while(j>=0 && array[j].CompareTo(key)>0)
+                {
+                    array[j + 1] = array[j];
+                    j = j - 1;
+                }
+                array[j + 1] = key;
+            }
+            print(array);
+            void print(string[] arr)
+            {
+                int n = array.Length;
+                for(int i=0;i<n;++i)
+                {
+                    Console.WriteLine(array[i] + " ");
+                }
+            }
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -9,8 +9,37 @@ namespace Algorithms
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO THE ALGORITHMS PROBLEM!");
-            BinarySearch search = new BinarySearch();
-            search.Find();
+            int defCount = 0;
+            while(defCount==0)
+            {
+                Console.WriteLine("1.FindWordBST 2.InsertionSort 3.BubbleSort 4.Exit");
+                Console.WriteLine("Choose Your Option");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch(choice)
+                {
+                    case 1:
+                        BinarySearch binary = new BinarySearch();
+                        binary.Find();
+                        break;
+
+                    case 2:
+                        InsertionSort insertion = new InsertionSort();
+                        insertion.Insert();
+                        break;
+
+                    case 3:
+                        BubbleSort bubble = new BubbleSort();
+                        bubble.BubbleSrt();
+                        break;
+
+                    default:
+                        Console.WriteLine("Exit");
+                        defCount++;
+                        break;
+
+                }
+
+            }
         }
     }
 }
